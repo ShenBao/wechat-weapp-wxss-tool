375b005752c5981e4d4045fe5f6032d1f04ab2ea
ShenBao <shenbaoone@gmail.com>
2017-09-20 12:06:20 +0800

    add wechat-wxss-tool

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wechat-wxss-tool
+# 微信小程序wxss小工具
+
 微信小程序wxss小工具，将scss转化为wxss
+
+(ps:稍后将更新readme)
+
+
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require("gulp-rename"),
+    glob = require('glob');
+
+gulp.task('sass', function () {
+  gulp.src('scss/*.scss')
+    .pipe(
+        sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('rename',function(){
+  let entries = function () {
+    let entryFiles = glob.sync('css/*.css')
+    let map = [];
+    for (let i = 0; i < entryFiles.length; i++) {
+        let filePath = entryFiles[i];
+        let filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'));
+        map[i] = filename;
+    }
+    return map;
+  };
+  let entriesCss = entries();
+  for (let index = 0; index < entriesCss.length; index++) {
+    console.log(entriesCss[index])
+    gulp.src("css/"+entriesCss[index]+".css")
+      .pipe(rename({
+        dirname: "xcx-source/pages/"+entriesCss[index],
+        extname: ".wxss"
+      }))
+      .pipe(gulp.dest("./"));
+  }
+
+});
+
+gulp.task('watch',['sass','rename'], function () {
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['rename']);
+});
+
+gulp.task('default', ['watch'], function () {
+  console.log('all task done!');
+});
